00296aa2306d526b467acf76d3a869e3b04bd464
André Varandas <andre.m.varandas@gmail.com>
2016-01-27 00:59:44 +0000

    feat(random): Add the ability to get an array of starwars names
    if passing a number to the random function, you will receive an array with that number of random
    items
    
    closes #2

--- src/index.test.js
+++ src/index.test.js
@@ -1,26 +1,34 @@
 var expect = require('chai').expect;
 var starWars = require('./index.js');
 
-describe('starwars-random-names', function () {
-	describe('all', function () {
-		it('should be an array of strings.', function () {
-			expect(starWars.all).to.satisfy(isArrayOfStrings);
+describe('starwars-random-names', function() {
+    describe('all', function() {
+        it('should be an array of strings.', function() {
+            expect(starWars.all).to.satisfy(isArrayOfStrings);
 
-			function isArrayOfStrings(array){
-				return array.every(function(item){
-					return typeof item === 'string';
-				})
-			}
-		});
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                })
+            }
+        });
+        it('Should contain Luke Skywalker', function() {
+            expect(starWars.all).to.include('Luke Skywalker');
+        });
+    });
 
-		it('Should contain Luke Skywalker', function () {
-			expect(starWars.all).to.include('Luke Skywalker');
-		});
+    describe('random', function() {
+        it('Should return a random value from the starWars.all', function() {
+            var randomValue = starWars.random();
+            expect(starWars.all).to.include(randomValue);
+        });
 
-		it('Should return a random value from the startWars.all', function () {
-			var randomValue = starWars.random();
-			expect(starWars.all).to.include(randomValue);
-		});
-
-	});
+        it('Should return an array of random names if passed a number', function() {
+            var randomItems = starWars.random(3);
+            expect(randomItems).to.have.length(3);
+            randomItems.forEach(function(item) {
+                expect(starWars.all).to.include(item);
+            });
+        });
+    });
 });
